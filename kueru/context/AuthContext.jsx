05c6d4137615f5902f9dc0bd1df4cbae8df472e7
malcomtyk21 <malcomtyk21@yahoo.com>
2026-03-22 16:00:36 +0800

    basic login landing page with authContext set up

---
+++ kueru/context/AuthContext.jsx
@@ -0,0 +1,31 @@
+/*
+Firebase App Listener - Firebase automatically checks if there is a user session
+*/
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "@/lib/firebase/config";
+
+const AuthContext = createContext(null);
+
+export function AuthProvider({ children }) {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setUser(user);
+            setLoading(false);
+        });
+        return () => unsubscribe();
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ user, loading }}>
+            {!loading && children}
+        </AuthContext.Provider>
+    );
+}
+
+export const useAuth = () => useContext(AuthContext);
